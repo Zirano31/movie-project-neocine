ece495172f0ab400e7c2821b263250f0fbbb9d58
Zirano31 <94104390+Zirano31@users.noreply.github.com>
2023-09-19 13:48:42 +0200

    Update Poster.jsx

--- src/components/singleMovie/Poster.jsx
+++ src/components/singleMovie/Poster.jsx
@@ -11,13 +11,20 @@ import SimpleContainer from "./poster_components/SimpleContainer"
 export default function Poster({ movie, credits }) {
   const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`
 
+  const direct = credits && credits.cast
+  ? credits.cast.find((member) => member.known_for_department === "Directing")
+  : null;
+  
+
+
+
   return (
     <>
       <SimpleContainer
         productionCompany={movie.production_companies}
         movie={movie}
         image={imageUrl}
-        director={credits}
+        direc={direct}
       ></SimpleContainer>
     </>
   )
