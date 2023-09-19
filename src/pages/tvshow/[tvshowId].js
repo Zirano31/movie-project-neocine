0cfec1645758bc160717ed46f50859c42df07cd6
Zirano31 <94104390+Zirano31@users.noreply.github.com>
2023-09-19 13:21:10 +0200

    Create [tvshowId].js

---
+++ src/pages/tvshow/[tvshowId].js
@@ -0,0 +1,39 @@
+import React from "react"
+import Poster from "@/components/singleMovie/Poster"
+import { fetcher } from "../../../util/API"
+import TrailerActorContainer from "@/components/singleMovie/Container"
+
+export default function showPage({ tvshow, credits, trai, similar }) {
+  const video = trai.results.find((result) => result)
+  const relatedshows = similar.results.map((result) => result)
+  return (
+    <>
+      <Poster movie={tvshow} />
+      <br></br>
+      <TrailerActorContainer
+        relatedMovies={relatedshows}
+        actors={credits}
+        video={video}
+      />
+    </>
+  )
+}
+
+// Define the getServerSideProps function to fetch data for a specific movie
+export async function getServerSideProps(context) {
+  const { tvshowId } = context.query
+
+  const tvshow = await fetcher(`/tv/${tvshowId}?language=en-US`)
+  const credits = await fetcher(`/tv/${tvshowId}/credits?language=en-US`)
+  const trailer = await fetcher(`/tv/${tvshowId}/videos?language=en-US`)
+  const similar = await fetcher(`/tv/${tvshowId}/similar?language=en-US&page=1`)
+
+  return {
+    props: {
+      tvshow: tvshow,
+      credits: credits,
+      trai: trailer,
+      similar: similar,
+    },
+  }
+}
